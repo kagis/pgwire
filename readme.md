fd1f207006829d7fbb3613c58e7209bc93d9aa50
Vladislav Nezhutin <exe-dealer@yandex.ru>
2019-01-13 08:09:24 +0600

    rewrite

--- readme.md
+++ readme.md
@@ -1,136 +1,61 @@
 # pgwire
 
-[![Build Status](https://travis-ci.com/kagis/pgwire.svg?branch=master)](https://travis-ci.com/kagis/pgwire)
-[![](https://img.shields.io/npm/v/pgwire.svg)](https://www.npmjs.com/package/pgwire)
+Under development
 
-Low level PostgreSQL client lib for Node.js
+[![npm](https://img.shields.io/npm/v/pgwire.svg)](https://www.npmjs.com/package/pgwire) [![travis](https://travis-ci.com/kagis/pgwire.svg?branch=master)](https://travis-ci.com/kagis/pgwire)
 
-- memory efficient data streaming
-- logical replication
-- pure js, zero dependencies
+PostgreSQL client library for Node.js
 
-## Simple query protocol
+## Features
 
-```js
-const { pgconnect } = require('pgwire');
-const conn = await pgconnect({ url: 'postgres://USER@HOST:PORT/DATABASE' });
-const cursor = conn.query(` SELECT 'hello', 'world' UNION ALL SELECT 'foo', 'bar' `);
-const results = await cursor.fetch();
-conn.terminate();
-const [first_result] = results;
-console.log(first_result.rows); // [['hello', 'world'], ['foo', 'bar']]
-```
+- Memory efficient data streaming
+- Logical replication using pgoutput protocol
+- Copy from stdin and to stdout
+- Multi-statement queries
+- True asynchronous
+- Pure js without dependencies
+<!-- - Bandwidth efficient binary transfering -->
+<!-- - Single round-trip requests for trusted pgBouncer connections -->
 
-## Extended query protocol
+## ![Examples](test/test.js)
 
-```js
-const { pgconnect } = require('pgwire');
-const conn = await pgconnect({ url: 'postgres://USER@HOST:PORT/DATABASE' });
-const cursor = (
-  conn
-  // prepare unnamed statement
-  .parse(` SELECT 'hello', $1 `)
-  // bind unnamed stamement to unnamed portal
-  .bind({ params: ['world'] })
-  // execute unnamed portal
-  .execute()
-  // end query batch
-  .sync()
-);
-const results = await cursor.fetch();
-const [first_result] = results;
-console.log(first_result.rows); // [['hello', 'world']]
-```
-
-## Multi-statement extended query
+- Simple query protocol
+- Extended query protocol
+- Multi-statement extended query
+- Execute many
+- Streaming
+- Multiplexing
+- Copy from stdin
+- Logical replication
 
 ```js
 const { pgconnect } = require('pgwire');
-const conn = await pgconnect({ url: 'postgres://USER@HOST:PORT/DATABASE' });
-const cursor = (
-  conn
-  .parse(`SELECT 'hello'`).bind().execute()
-  .parse(`SELECT 'world'`).bind().execute()
-  .sync()
-);
-const results = await cursor.fetch();
-const [first_result, second_results] = results;
-console.log(first_result.rows); // [['hello']]
-console.log(second_result.rows); // [['world']]
+const conn = await pgconnect({ url: 'postgres://<user>@<host>:<port>/<database>' });
+const { rows } = await conn.query(`SELECT 'hello', 'world'`);
+console.log(rows);
+// [['hello', 'world']]
 ```
 
-## Execute many
+## API Reference
 
-```js
-const { pgconnect } = require('pgwire');
-const conn = await pgconnect({ url: 'postgres://USER@HOST:PORT/DATABASE' });
-const cursor = (
-  conn
-  .parse(`SELECT 'hello', $1`)
-  .bind({ params: ['world'] })
-  .execute()
-  .bind({ params: ['other'] })
-  .execute()
-  .sync()
-);
-const results = await cursor.fetch();
-const [first_result, second_results] = results;
-console.log(first_result.rows); // [['hello', 'world']]
-console.log(second_result.rows); // [['hello', 'other']]
-```
-
-## Streaming
-
-```js
-const { pgconnect } = require('pgwire');
-const conn = await pgconnect({ url: 'postgres://USER@HOST:PORT/DATABASE' });
-const cursor = (
-  conn
-  .parse(`SELECT generate_series(0, 999999)`)
-  .bind()
-  .execute()
-  .sync()
-);
-for await (const msg of cursor) {
-  if (msg.tag == 'DataRow') {
-    console.log(msg.data);
-  }
-}
-// [['0']]
-// [['1']]
-// [['2']]
-// ...
-```
-
-## Multiplexing
-
-```js
-const { pgconnect } = require('pgwire');
-const conn = await pgconnect({ url: 'postgres://USER@HOST:PORT/DATABASE' });
-
-await (
-  conn
-  // start explicit transaction to preserve portals between Sync commands
-  .parse('BEGIN').bind().execute()
-  .parse({ name: 'stmt50', statement: `SELECT generate_series( 1, 50)` })
-  .bind({ name: 'stmt50', portal: 'portal50' })
-  .parse({ name: 'stmt99', statement: `SELECT generate_series(50, 99)` })
-  .bind({ name: 'stmt99', portal: 'portal99' })
-  .sync()
-  .fetch()
-);
-
-let some_portal_was_suspended;
-do {
-  const [first_result, second_result] = await (
-    conn
-    .execute({ portal: 'portal50', limit: 10 })
-    .execute({ portal: 'portal99', limit: 10 })
-    .sync()
-    .fetch()
-  );
-  console.log(first_result.rows);
-  console.log(second_result.rows);
-  some_portal_was_suspended = first_result.suspended || second_result.suspended;
-} while (some_portal_was_suspended);
-```
+- pgconnect()
+  - .query()
+  - .queryStream()
+  - .extendedQuery()
+    - .parse()
+    - .bind()
+    - .execute()
+    - .describeStatement()
+    - .describePortal()
+    - .closeStatement()
+    - .closePortal()
+    - .fetch()
+    - .stream()
+  - .end()
+  - .logicalReplication()
+    - .pgoutput()
+    - .ack()
+    - .ackImmediate()
+    - .end()
+- pgliteral()
+- pgident()
--- test/test.js
+++ test/test.js
@@ -1,236 +1,221 @@
-const { deepEqual } = require('assert');
+const { deepStrictEqual } = require('assert');
 const { Readable } = require('stream');
+const { execSync } = require('child_process');
 const { pgconnect } = require('../lib/connect.js');
 
-// it('logical replication pgoutput', async _ => {
-//   const conn = await pgconnect();
+it('logical replication', async _ => {
+  psql(/*sql*/ `
+    SELECT pg_create_logical_replication_slot('test', 'test_decoding');
+    CREATE TABLE foo AS SELECT 1 a;
+  `);
+  const expected = JSON.parse(psql(/*sql*/ `
+    SELECT json_agg(jsonb_build_object(
+      'lsn', lpad(split_part(lsn::text, '/', 1), 8, '0') ||
+        '/' || lpad(split_part(lsn::text, '/', 2), 8, '0'),
+      'data', data
+    ))
+    FROM pg_logical_slot_peek_changes('test', NULL, NULL)
+  `));
+  const conn = await pgconnect({
+    replication: 'database',
+  });
+  const replstream = await conn.logicalReplication({
+    slot: 'test',
+    startLsn: '0/0',
+  });
+  const lines = [];
+  const timer = setTimeout(_ => replstream.end(), 500);
+  for await (const { lsn, data } of replstream) {
+    lines.push({ lsn, data: data.toString() });
+    timer.refresh();
+  }
+  conn.end();
+  deepStrictEqual(lines, expected);
+});
 
-//   // const stream = conn.query('COPY (VALUES(1,2)) TO STDOUT; COPY (VALUES(3,4)) TO STDOUT');
-//   // for await (const msg of stream) {
-//   //   console.log(msg);
-//   // }
-//   try {
+it('CREATE_REPLICATION_SLOT issue', async _ => {
+  const conn = await pgconnect({
+    replication: 'database',
+  });
+  await Promise.all([
+    conn.query('CREATE_REPLICATION_SLOT crs_iss LOGICAL test_decoding'),
+    conn.query('CREATE_REPLICATION_SLOT crs_iss LOGICAL test_decoding').catch(_ => null),
+    conn.query('SELECT 1'),
+    conn.end(),
+  ]);
+});
 
-//     const stream = conn._tx;
-//     stream.write({
-//       tag: 'Parse',
-//       statement: 'select 1 / (10 - i) from generate_series(1, 10) i',
-//     });
-//     stream.write({
-//       tag: 'Bind',
-//     });
-//     stream.write({
-//       tag: 'Execute',
-//       limit: 1,
-//     });
-//     stream.write({
-//       tag: 'Query',
-//       query: 'SELECT 1_',
-//     });
-//     // stream.write({
-//     //   tag: 'Sync',
-//     // });
-//     // stream.end();
+it('logical replication pgoutput', async _ => {
+  psql(/*sql*/ `
+    BEGIN;
+    CREATE TABLE foo1(a INT NOT NULL PRIMARY KEY, b TEXT);
+    -- ALTER TABLE foo1 REPLICA IDENTITY FULL;
+    CREATE PUBLICATION pub1 FOR TABLE foo1;
+    COMMIT;
 
-//     // const stream = conn.query(`
-//     //   create table hello(id int);
-//     //   -- select generate_series(0, 100);
-//     //   -- COPY (VALUES(1,2)) TO STDOUT;
-//     //   COPY hello from stdout;
-//     //   COPY hello from stdout;
-//     //   table hello;
-//     // `);
-//     // const stream = (
-//     //   conn
-//     //   .parse('create table hello(id int)').bind().execute()
-//     //   .parse('COPY hello from stdout').bind().execute()
-//     //   // .parse('COPY hello from stdout').bind().execute()
-//     //   // .parse('table hello').bind().execute()
-//     //   // .copydata('he\n')
-//     //   // .copydata('1\n2\n')
-//     //   // .copydone()
-//     //   .sync()
-//     // );
-//     // stream.write();
-//     // conn.copydata('hello\nhello\n');
-//     // conn.copydata('1\n2\n');
-//     // conn.copydone();
-//     // conn.copydata('3\n4\n');
-//     // conn.copydone();
-//     // const cur = conn._cursor();
-//     // // cur.on('error', console.log);
-//     for await (const msg of conn._rx) {
-//       console.log(msg);
-//     }
+    SELECT pg_create_logical_replication_slot('test1', 'pgoutput');
 
-//   } finally {
-//     // conn.terminate();
-//   }
+    BEGIN;
+    INSERT INTO foo1 VALUES (1, 'hello'), (2, 'world');
+    UPDATE foo1 SET b = 'all' WHERE a = 1;
+    DELETE FROM foo1 WHERE a = 2;
+    TRUNCATE foo1;
+    COMMIT;
+  `);
+  const expectedRelation = {
+    relationid: Number(psql(/*sql*/ `SELECT 'public.foo1'::regclass::oid`)),
+    schema: 'public',
+    name: 'foo1',
+    replicaIdentity: 'd',
+    attrs: [
+      { flags: 1, name: 'a', typeid: 23, typemod: -1 },
+      { flags: 0, name: 'b', typeid: 25, typemod: -1 },
+    ],
+  };
+  const peekedChanges = JSON.parse(psql(/*sql*/ `
+    SELECT json_agg(jsonb_build_object(
+      'lsn', lpad(split_part(lsn::text, '/', 1), 8, '0') ||
+        '/' || lpad(split_part(lsn::text, '/', 2), 8, '0'),
+      'xid', xid::text::int
+    ))
+    FROM pg_logical_slot_peek_binary_changes('test1', NULL, NULL,
+      'proto_version', '1', 'publication_names', 'pub1')
+  `));
+  const conn = await pgconnect({
+    replication: 'database',
+  });
+  const replstream = await conn.logicalReplication({
+    slot: 'test1',
+    startLsn: '0/0',
+    options: {
+      'proto_version': 1,
+      'publication_names': 'pub1',
+    },
+  });
+  const timer = setTimeout(_ => replstream.end(), 500);
+  const pgomsgs = [];
+  for await (const pgomsg of replstream.pgoutput()) {
+    delete pgomsg.endLsn;
+    delete pgomsg.time;
+    delete pgomsg.finalLsn;
+    delete pgomsg.commitTime;
+    delete pgomsg.commitLsn;
+    delete pgomsg._endLsn;
+    pgomsgs.push(pgomsg);
+    timer.refresh();
+  }
+  conn.end();
+  deepStrictEqual(pgomsgs, [{
+    tag: 'begin',
+    xid: peekedChanges[0].xid,
+    lsn: peekedChanges.shift().lsn,
+  }, {
+    tag: 'relation',
+    lsn: (peekedChanges.shift().lsn, '00000000/00000000'), // why?
+    ...expectedRelation,
+  }, {
+    tag: 'insert',
+    lsn: peekedChanges.shift().lsn,
+    after: ['1', 'hello'],
+    relation: expectedRelation,
+  }, {
+    tag: 'insert',
+    lsn: peekedChanges.shift().lsn,
+    after: ['2', 'world'],
+    relation: expectedRelation,
+  }, {
+    tag: 'update',
+    lsn: peekedChanges.shift().lsn,
+    relation: expectedRelation,
+    before: null,
+    after: ['1', 'all'],
+  }, {
+    tag: 'delete',
+    lsn: peekedChanges.shift().lsn,
+    relation: expectedRelation,
+    keyOnly: true,
+    before: ['2', null],
+  }, {
+    tag: 'relation',
+    lsn: (peekedChanges.shift().lsn, '00000000/00000000'), // why?
+    ...expectedRelation,
+  }, {
+    tag: 'truncate',
+    lsn: peekedChanges.shift().lsn,
+    cascade: false,
+    restartSeqs: false,
+    relations: [expectedRelation],
+  }, {
+    tag: 'commit',
+    lsn: peekedChanges.shift().lsn,
+    flags: 0,
+  }]);
+});
 
-
-//   // const [_parse, _bind, result] = await Promise.all([
-//   //   conn.parse('create table'),
-//   //   conn.bind(),
-//   //   conn.execute(),
-//   // ]);
-
-//   // const repltx = new PassThrough();
-//   // const replrx = await (
-//   //   conn
-//   //   .query({
-//   //     sql: 'START_REPLICATION SLOT hello LOGICAL 0/0',
-//   //     copyFrom: repltx,
-//   //   })
-//   // );
-
-//   const replstream = await conn.startReplication('START_REPLICATION SLOT hello LOGICAL 0/0');
-//   replstream.pipe(new PgOutputLogicalDecoder());
-//   replstream.ack(lsn);
-
-//   const response = conn.query({
-//     sql: /*sql*/ `
-//       CREATE TABLE tbl(foo int4);
-//       COPY tbl FROM STDIN;
-//       COPY tbl FROM STDIN;
-//       SELECT * FROM tbl;
-//     `,
-//     copyFrom(descr) {
-
-//     },
-//     copyFrom: [
-//       arrstream([
-//         '1\n2\n',
-//         '3\n4\n',
-//       ]),
-//     ],
-//   });
-//   const a = await response.fetch();
-
-
-//   // conn.query([{
-//   //   tag: 'parse',
-//   // }, {
-//   //   tag: 'bind'
-//   // }])
-
-//   const results = await (
-//     conn.extendedQuery()
-//     .parse('COPY hello FROM STDIN')
-//     .bind()
-//     .execute() // should throw unexpected_copyin
-//     .parse('COPY tbl FROM STDIN')
-//     .bind()
-//     .execute({
-//       // consume(output, results) {
-//       //   // что если мне здесь нужен результат предыдущей команды ?
-//       //   throw 'can throw - should terminate or skip until Z';
-//       // },
-//       copyFrom: arrstream([
-//         '1\n2\n',
-//         '3\n4\n',
-//       ]),
-//     })
-//     .parse('SELECT 1')
-//     .bind()
-//     .execute()
-//     .sync()
-//   );
-
-//   // for await (const result of results) {
-
-//   // }
-
-
-
-//   const { transaction_status } = await conn.sync();
-
-//   process.exit(0);
-// });
-
-// it('logical replication', async _ => {
-//   const conn = await pgconnect({
-//     replication: 'database',
-//   });
-
-//   await conn.query(`CREATE_REPLICATION_SLOT testslot LOGICAL test_decoding`).fetch();
-//   await conn.query(`CREATE TABLE hello AS SELECT 'hello' foo`);
-
-//   const repl_stream = (
-//     conn
-//     .query(`START_REPLICATION SLOT testslot LOGICAL 0/0`)
-//     .replication()
-//   );
-
-//   const changes = [];
-//   for await (const repl_msg of repl_stream) {
-//     const change = repl_msg.data.toString();
-//     if (/^COMMIT/.test(change)) {
-//       changes.push('COMMIT');
-//       break;
-//     } else if (/^BEGIN/.test(change)) {
-//       changes.push('BEGIN');
-//     } else {
-//       changes.push(change);
-//     }
-//   }
-//   conn.terminate();
-
-//   deepEqual(changes, [
-//     `BEGIN`,
-//     `table public.hello: INSERT: foo[text]:'hello'`,
-//     `COMMIT`,
-//   ]);
-// });
+xit('logical replication ack'); // todo
 
 it('simple proto', async _ => {
   const conn = await pgconnect();
-  const result = await conn.query(`SELECT 1`).fetch();
-  conn.terminate();
+  const response = conn.query(`SELECT 1`);
+  conn.end();
 
-  deepEqual(result, [{
+  deepStrictEqual(await response, {
+    inTransaction: false,
     rows: [['1']],
-    commandtag: 'SELECT 1',
-    suspended: false,
     empty: false,
-  }]);
+    suspended: false,
+    scalar: '1',
+    command: 'SELECT 1',
+    results: [{
+      rows: [['1']],
+      command: 'SELECT 1',
+    }],
+  });
 });
 
 it('simple proto multi stmt', async _ => {
   const conn = await pgconnect();
-  const result = await conn.query(`VALUES (1), (2); VALUES ('a', 'b');`).fetch();
-  conn.terminate();
-
-  deepEqual(result, [{
-    rows: [['1'], ['2']],
-    commandtag: 'SELECT 2',
-    suspended: false,
+  const response = conn.query(`VALUES (1), (2); VALUES ('a', 'b');`);
+  conn.end();
+  deepStrictEqual(await response, {
+    inTransaction: false,
+    results:[{
+      rows: [['1'], ['2']],
+      command: 'SELECT 2',
+    }, {
+      rows: [['a', 'b']],
+      command: 'SELECT 1',
+    }],
+    rows: [[ 'a', 'b' ]],
+    scalar: 'a',
+    command: 'SELECT 1',
     empty: false,
-  }, {
-    rows: [['a', 'b']],
-    commandtag: 'SELECT 1',
     suspended: false,
-    empty: false,
-  }]);
+  });
 });
 
 it('extended proto', async _ => {
   const conn = await pgconnect();
-  const result = await (
+  const response = (
     conn.extendedQuery()
     .parse(`SELECT 1`)
     .bind()
     .execute()
-    .sync()
     .fetch()
   );
-  conn.terminate();
-
-  deepEqual(result, [{
+  conn.end();
+  deepStrictEqual(await response, {
+    inTransaction: false,
+    results: [{
+      rows: [['1']],
+      command: 'SELECT 1',
+    }],
     rows: [['1']],
-    commandtag: 'SELECT 1',
-    suspended: false,
+    scalar: '1',
+    command: 'SELECT 1',
     empty: false,
-  }]);
+    suspended: false,
+  });
 });
 
 it('query queue', async _ => {
@@ -240,71 +225,90 @@ it('query queue', async _ => {
     .parse(`SELECT 1`)
     .bind()
     .execute()
-    .sync()
+    .fetch()
   );
   const response2 = (
     conn.extendedQuery()
     .parse(`SELECT 2`)
     .bind()
     .execute()
-    .sync()
+    .fetch()
   );
-  const [result1, result2] = await Promise.all([
-    response1.fetch(),
-    response2.fetch(),
+  conn.end();
+  const result = await Promise.all([
+    response1,
+    response2,
   ]);
-  conn.terminate();
 
-  deepEqual(result1, [{
+  deepStrictEqual(result, [{
+    inTransaction: false,
+    results: [{
+      rows: [['1']],
+      command: 'SELECT 1',
+    }],
     rows: [['1']],
-    commandtag: 'SELECT 1',
-    suspended: false,
+    scalar: '1',
+    command: 'SELECT 1',
     empty: false,
-  }]);
-  deepEqual(result2, [{
+    suspended: false,
+  }, {
+    inTransaction: false,
+    results: [{
+      rows: [['2']],
+      command: 'SELECT 1',
+    }],
     rows: [['2']],
-    commandtag: 'SELECT 1',
-    suspended: false,
+    scalar: '2',
+    command: 'SELECT 1',
     empty: false,
+    suspended: false
   }]);
 });
 
 it('portal suspended', async _ => {
   const conn = await pgconnect();
-  const result = await (
+  const response = (
     conn.extendedQuery()
     .parse(`SELECT generate_series(0, 10)`)
     .bind()
     .execute({ limit: 2 })
-    .sync()
     .fetch()
   );
-  conn.terminate();
-  deepEqual(result, [{
+  conn.end();
+  deepStrictEqual(await response, {
+    inTransaction: false,
+    results: [{
+      rows: [['0'], ['1']],
+      suspended: true,
+    }],
     rows: [['0'], ['1']],
-    commandtag: undefined,
-    suspended: true,
+    scalar: '0',
+    command: undefined,
     empty: false,
-  }]);
+    suspended: true,
+  });
 });
 
 it('empty query', async _ => {
   const conn = await pgconnect();
-  const result = await (
+  const response = (
     conn.extendedQuery()
-    .parse(``)
-    .bind()
-    .execute()
-    .sync()
+    .parse(``).bind().execute()
     .fetch()
   );
-  conn.terminate();
-  deepEqual(result, [{
+  conn.end();
+  deepStrictEqual(await response, {
+    inTransaction: false,
+    results: [{
+      rows: [],
+      empty: true,
+    }],
     rows: [],
-    commandtag: undefined,
-    suspended: false,
+    scalar: undefined,
+    command: undefined,
     empty: true,
-  }]);
+    suspended: false,
+  });
 });
 
 it('multiplexing', async _ => {
@@ -317,27 +321,25 @@ it('multiplexing', async _ => {
     .bind({ name: 'stmt1', portal: 'portal1' })
     .parse({ name: 'stmt2', statement: `SELECT generate_series(20, 29)` })
     .bind({ name: 'stmt2', portal: 'portal2' })
-    .sync()
     .fetch()
   );
 
   let some_portal_was_suspended;
   const elems = [];
   do {
-    const [first_result, second_result] = await (
+    const { results: [first_result, second_result] } = await (
       conn.extendedQuery()
       .execute({ portal: 'portal1', limit: 2 })
       .execute({ portal: 'portal2', limit: 2 })
-      .sync()
       .fetch()
     );
     elems.push(...first_result.rows.map(([it]) => it))
     elems.push(...second_result.rows.map(([it]) => it))
     some_portal_was_suspended = first_result.suspended || second_result.suspended;
   } while (some_portal_was_suspended);
-  conn.terminate();
+  conn.end();
 
-  deepEqual(elems, [
+  deepStrictEqual(elems, [
     '10', '11', '20', '21',
     '12', '13', '22', '23',
     '14', '15', '24', '25',
@@ -348,7 +350,7 @@ it('multiplexing', async _ => {
 
 it('simple copy in', async _ => {
   const conn = await pgconnect();
-  const result = await conn.query({
+  const response = conn.query({
     sql: /*sql*/ `
       BEGIN;
       CREATE TABLE test(foo INT, bar TEXT);
@@ -356,19 +358,18 @@ it('simple copy in', async _ => {
       SELECT * FROM test;
     `,
     stdin: arrstream(['1\t', 'hello\n', '2\t', 'world\n']),
-  }).fetch();
-  conn.terminate();
-  deepEqual(result[3], {
-    rows: [['1', 'hello'], ['2', 'world']],
-    commandtag: 'SELECT 2',
-    suspended: false,
-    empty: false,
   });
+  conn.end();
+  const { rows } = await response;
+  deepStrictEqual(rows, [
+    ['1', 'hello'],
+    ['2', 'world']
+  ]);
 });
 
 it('extended copy in', async _ => {
   const conn = await pgconnect();
-  const result = await (
+  const response = (
     conn.extendedQuery()
     .parse('BEGIN').bind().execute()
     .parse('CREATE TEMP TABLE test(foo INT, bar TEXT)').bind().execute()
@@ -376,48 +377,67 @@ it('extended copy in', async _ => {
       stdin: arrstream(['1\t', 'hello\n', '2\t', 'world\n']),
     })
     .parse('SELECT * FROM test').bind().execute()
-    .sync()
     .fetch()
   );
-  conn.terminate();
-  deepEqual(result[3], {
-    rows: [['1', 'hello'], ['2', 'world']],
-    commandtag: 'SELECT 2',
-    suspended: false,
-    empty: false,
-  });
+  conn.end();
+  const { rows } = await response;
+  deepStrictEqual(rows, [
+    ['1', 'hello'],
+    ['2', 'world'],
+  ]);
 });
 
 it('extended copy in missing 1', async _ => {
   const conn = await pgconnect();
-  const result = await (
+  const response = (
     conn.extendedQuery()
     .parse('BEGIN').bind().execute()
     .parse('CREATE TEMP TABLE test(foo INT, bar TEXT)').bind().execute()
     .parse('COPY test FROM STDIN').bind().execute()
-    .sync()
     .fetch()
     .catch(err => err.code)
   );
-  conn.terminate();
-  deepEqual(result, 'PGERROR_MISSING_STDIN');
+  conn.end();
+  deepStrictEqual(await response, 'PGERR_57014');
 });
 
-function it(testname, fn) {
+it('simple copy in missing', async _ => {
+  const conn = await pgconnect();
+  const response = conn.query({
+    sql: /*sql*/ `
+      BEGIN;
+      CREATE TABLE test(foo INT, bar TEXT);
+      COPY test FROM STDIN;
+      SELECT 'hello';
+      COPY test FROM STDIN;
+      COPY test FROM STDIN;
+
+    `,
+    stdin: [
+      arrstream(['1\t', 'hello\n', '2\t', 'world\n']),
+      arrstream(['1\t', 'hello\n', '2\t', 'world\n']),
+      // here should be third one
+    ],
+  });
+  conn.end();
+  deepStrictEqual(await response.catch(err => err.code), 'PGERR_57014');
+});
+
+function xit() {}
+function it(name, fn) {
   it.tests = it.tests || [];
-  it.tests.push({ testname, fn });
+  it.tests.push({ name, fn });
 }
 async function main() {
-  for (const { testname, fn } of it.tests) {
+  for (const { name, fn } of it.tests) {
     await fn();
-    console.log(testname, '- ok');
+    console.log(name, '- ok');
   }
 }
 main().catch(err => {
   console.error(err);
   process.exitCode = 1;
 });
-
 
 function arrstream(chunks) {
   return Readable({
@@ -426,4 +446,11 @@ function arrstream(chunks) {
       this.push(null);
     }
   })
+}
+
+function psql(input) {
+  return (
+    execSync(`psql "${process.env.POSTGRES}" -Atc $'${input.replace(/'/g, '\\\'')}'`)
+    .toString()
+  );
 }
