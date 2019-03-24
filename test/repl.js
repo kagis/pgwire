57c7c671a96b7d5ccd5feafd9d7f174c5965a90a
Vladislav Nezhutin <exe-dealer@yandex.ru>
2019-03-25 05:12:15 +0600

    fix repl tool

--- test/repl.js
+++ test/repl.js
@@ -1,14 +1,14 @@
 const net = require('net');
 const repl = require('repl');
 const { Writable } = require('stream');
-const { BackendDecoder } = require('../lib/backend.js');
-const fe = require('../lib/frontend.js');
+const BackendDecoder = require('../lib/backend.js');
+const { FrontendEncoder, ...fe } = require('../lib/frontend.js');
 
 const socket = net.connect({
   host: 'postgres', port: 5432,
   // host: 'pgbouncer', port: 6432,
 });
-const tx = new fe.FrontendEncoder();
+const tx = new FrontendEncoder();
 tx.pipe(socket).pipe(new BackendDecoder()).pipe(new Writable({
   objectMode: true,
   write(message, _enc, done) {
